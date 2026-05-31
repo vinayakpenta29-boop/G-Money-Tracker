9623cc6622b5adb20eab4eb7aa18d1782a5c9126
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-06-01 01:04:03 +0530

    Update DatabaseHelper.java

--- app/src/main/java/com/example/moneytracker/DatabaseHelper.java
+++ app/src/main/java/com/example/moneytracker/DatabaseHelper.java
@@ -11,8 +11,9 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "MoneyTracker.db";
-    private static final int DATABASE_VERSION = 2; // Incremented version
-    private static final String TABLE_NAME = "transactions";
+    private static final int DATABASE_VERSION = 3; // Upgraded version
+    private static final String TABLE_TRANSACTIONS = "transactions";
+    private static final String TABLE_MEMBERS = "members";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,17 +21,42 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Added DATE TEXT
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, PERSON TEXT, AMOUNT REAL, TYPE TEXT, DATE TEXT)";
-        db.execSQL(createTable);
+        String createTransactions = "CREATE TABLE " + TABLE_TRANSACTIONS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, PERSON TEXT, AMOUNT REAL, TYPE TEXT, DATE TEXT)";
+        String createMembers = "CREATE TABLE " + TABLE_MEMBERS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT UNIQUE)";
+        db.execSQL(createTransactions);
+        db.execSQL(createMembers);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TRANSACTIONS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEMBERS);
         onCreate(db);
     }
 
+    // --- MEMBER METHODS ---
+    public boolean addMember(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME", name);
+        long result = db.insert(TABLE_MEMBERS, null, contentValues);
+        return result != -1;
+    }
+
+    public List<String> getAllMembers() {
+        List<String> list = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT NAME FROM " + TABLE_MEMBERS + " ORDER BY NAME ASC", null);
+        if (cursor.moveToFirst()) {
+            do {
+                list.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    // --- TRANSACTION METHODS ---
     public boolean addTransaction(String person, double amount, String type, String date) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -38,26 +64,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put("AMOUNT", amount);
         contentValues.put("TYPE", type);
         contentValues.put("DATE", date);
-        long result = db.insert(TABLE_NAME, null, contentValues);
+        long result = db.insert(TABLE_TRANSACTIONS, null, contentValues);
         return result != -1;
     }
 
     public void deleteTransaction(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_NAME, "ID=?", new String[]{String.valueOf(id)});
+        db.delete(TABLE_TRANSACTIONS, "ID=?", new String[]{String.valueOf(id)});
     }
 
-    // Get ALL transactions
     public List<Transaction> getAllTransactions() {
-        return queryDatabase("SELECT * FROM " + TABLE_NAME + " ORDER BY ID DESC");
+        return queryDatabase("SELECT * FROM " + TABLE_TRANSACTIONS + " ORDER BY ID DESC");
     }
 
-    // Get transactions for a SPECIFIC person
     public List<Transaction> getTransactionsByPerson(String personName) {
-        return queryDatabase("SELECT * FROM " + TABLE_NAME + " WHERE PERSON = '" + personName + "' ORDER BY ID DESC");
+        return queryDatabase("SELECT * FROM " + TABLE_TRANSACTIONS + " WHERE PERSON = '" + personName + "' ORDER BY ID DESC");
     }
 
-    // Helper method to read the cursor
     private List<Transaction> queryDatabase(String query) {
         List<Transaction> list = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
